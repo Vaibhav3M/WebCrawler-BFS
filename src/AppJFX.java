2354753b9d3fcae0cd9005f39d9d671f4e8622d5
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-07-11 16:29:19 -0400

    Basic JFX interface with console output

--- src/AppJFX.java
+++ src/AppJFX.java
@@ -1,12 +1,6 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class AppJFX extends Application {
 
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
+public class AppJFX  {
 
 
-    }
 }
--- src/view/Welcome.java
+++ src/view/Welcome.java
@@ -1,6 +1,46 @@
 package view;
 
-public class Welcome {
+import Model.BFSCrawler;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Welcome extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        TextArea welcomeText = new TextArea();
+        welcomeText.setText("Enter a valid URL");
+
+        TextField url = new TextField();
+
+
+        Button go = new Button("GO");
+        go.setOnMouseClicked(event -> {
+            BFSCrawler crawler = new BFSCrawler();
+            crawler.callCrawler(url.getText());
+        });
+
+        VBox vbox = new VBox();
+
+        vbox.getChildren().addAll(welcomeText,url,go);
+
+        Scene scene  = new Scene(vbox,500,500);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
 
 
 
