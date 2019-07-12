21f7ece066e88beeeccfc5a13cf2ce04fb35d47b
Vaibhav3M <44673587+Vaibhav3M@users.noreply.github.com>
2019-07-12 19:47:57 -0400

    URL validation

--- src/view/Welcome.java
+++ src/view/Welcome.java
@@ -10,6 +10,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Welcome extends Application {
 
 
@@ -19,6 +22,7 @@ public class Welcome extends Application {
 
         TextArea welcomeText = new TextArea();
         welcomeText.setText("Enter a valid URL");
+        welcomeText.setEditable(false);
 
         TextField url = new TextField();
 
@@ -26,7 +30,10 @@ public class Welcome extends Application {
         Button go = new Button("GO");
         go.setOnMouseClicked(event -> {
             BFSCrawler crawler = new BFSCrawler();
-            crawler.callCrawler(url.getText());
+            String urlString = formatURL(url.getText());
+
+            System.out.println(urlString);
+            crawler.callCrawler(urlString);
         });
 
         VBox vbox = new VBox();
@@ -37,6 +44,29 @@ public class Welcome extends Application {
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+    }
+
+    private String formatURL(String url){
+
+        String regexp1 = "www.";
+        Pattern pattern1 = Pattern.compile(regexp1);
+        Matcher matcher1 = pattern1.matcher(url);
+
+        if (!matcher1.find()){
+            url = "www." + url;
+        }
+
+        String regexp2 = "https://";
+        Pattern pattern2 = Pattern.compile(regexp2);
+        Matcher matcher2 = pattern2.matcher(url);
+
+
+        if (!matcher2.find()){
+            url = "https://" + url;
+        }
+
+        return url;
 
     }
 
